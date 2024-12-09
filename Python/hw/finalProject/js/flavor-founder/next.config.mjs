d3f4152741efcc1561d6a593c685c8da560817c3
Anastasiia Rusakova <arusakova917@gmail.com>
2024-12-09 14:27:50 +0200

    Final project moved to separate repo

--- Python/hw/finalProject/js/flavor-founder/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['cosylab.iiitd.edu.in'], // Add your external hostname here
-    },
-};
-
-
-export default nextConfig;
