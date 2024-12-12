b3d1677ff2a4ed58f40d51e3dbce7302d515d2fe
Anastasiia Rusakova <alikocold@gmail.com>
2024-12-12 17:26:43 +0200

    Added 2-nd part of the hw - with string parsing

---
+++ Databases/hw5/string_parsing.sql
@@ -0,0 +1,9 @@
+﻿DECLARE @filename AS NVARCHAR(255) = N'C:\Users\aliko\Dropbox\Збірник рецептур нац. страв. Шалимінов О.В. 2007\Страви з картоплі овочів грибів.pdf';
+
+SELECT 
+	type = CASE
+	WHEN PATINDEX('%:%', value) > 0 THEN 'Drive ->'
+	WHEN PATINDEX('%.%', value) > 0 THEN 'File -> '
+	ELSE 'Folder ->'
+	END,
+	value FROM string_split(@filename, '\')
