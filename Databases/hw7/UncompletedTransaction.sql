1e8bc83d15c671acc33d243911fc883194394a72
Anastasiia Rusakova <alikocold@gmail.com>
2025-01-10 13:27:24 +0200

    Homework 7: Uncompleted transaction with read isolation

---
+++ Databases/hw7/UncompletedTransaction.sql
@@ -0,0 +1,33 @@
+USE AdventureWorks2022;
+GO
+
+SELECT * 
+FROM [Production].Product
+ORDER BY ListPrice ASC
+
+BEGIN TRAN
+
+UPDATE price
+SET ListPrice=1
+FROM [Production].Product price
+WHERE ProductID=1
+
+BEGIN TRAN
+COMMIT TRAN
+
+
+SELECT * 
+FROM [Production].Product
+WHERE ProductID=1
+ORDER BY ListPrice ASC
+
+BEGIN TRAN
+
+UPDATE price
+SET ListPrice=2
+FROM [Production].Product price
+WHERE ProductID=1
+
+
+
+
